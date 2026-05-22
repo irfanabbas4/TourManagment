5e6e20d24e88bd6422c2cb2b83ea82f8bc47b8b4
HP <irfanabbasbaqdool@gmail.com>
2026-05-22 12:13:58 -0700

    chore: fix yaml

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
+
+import { StrictMode } from "react"
 import App from "./App.tsx";
 
-import { StrictMode } from "react";
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<App />
